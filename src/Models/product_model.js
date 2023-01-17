4da2e02631e07ef4abafb865311eddda370c88b7
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-01-16 23:14:24 -0300

    user model error

--- src/Models/product_model.js
+++ src/Models/product_model.js
@@ -1,59 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const reviewSchema= mongoose.Schema({
+const reviewSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    rating:{
-        type:Number,
-        required: true,
+    rating: {
+      type: Number,
+      required: true,
     },
     comment: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "UserModel"
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "UserModel",
     },
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const ProductModel = mongoose.Schema({
+const ProductModel = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    image:{
-        type: String,
-        required: true
+    image: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
-        required: true
+    description: {
+      type: String,
+      required: true,
     },
-    reviews:[reviewSchema],
-    rating:{
-        type:Number,
-        required: true,
-        default: 0
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
     },
-    numReviews:{
-        type:Number,
-        required: true,
-        default: 0
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
     },
-    price:{
-        type:Number,
-        required: true,
-        default: 0
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
     },
-    countInStock:{
-        type:Number,
-        required: true,
-        default: 0
-    }
-})
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("ProductModel", ProductModel)
+module.exports = mongoose.model("ProductModel", ProductModel);
