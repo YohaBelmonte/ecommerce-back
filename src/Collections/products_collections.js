fc306ed1dfbd9d3bfd128c6b6f3200aef1e4a76c
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-01-26 20:27:25 -0300

    update admin falta function delete

--- src/Collections/products_collections.js
+++ src/Collections/products_collections.js
@@ -1,7 +1,7 @@
 const ProductModel = require("../Models/product_model");
-const UserModel = require("../Models/user_model");  
+const UserModel = require("../Models/user_model");
 
-//GET ALLS PRODUCT 
+//GET ALLS PRODUCT
 exports.GetProduct = async (req, res) => {
   try {
     const response = await ProductModel.find();
@@ -13,53 +13,55 @@ exports.GetProduct = async (req, res) => {
   }
 };
 
-//GET SINGLE PRODUCT 
-exports.GetOneProduct = async (req, res) => {
-  try {
-    const { idProduct } = req.params;
-    const response = await ProductModel.findById(idProduct);
-    console.log(idProduct)
-    res.status(200).send(response);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send("hubo un error en la peticion get");
-  }
-};
-
+//GET SINGLE PRODUCT
+// exports.GetOneProduct = async (req, res) => {
+//   try {
+//     const { idProduct } = req.params;
+//     const response = await ProductModel.findById(idProduct);
+//     console.log(idProduct)
+//     res.status(200).send(response);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).send("hubo un error en la peticion get");
+//   }
+// };
 
 exports.PostProduct = async (req, res) => {
-  const {name, image, description, price, countInStock} = req.body;
+  const { name, image, description, price, countInStock } = req.body;
   try {
-    const userModel= await UserModel.findById(req.usuario.id)
+    const userModel = await UserModel.findById(req.usuario.id);
 
     const model = new ProductModel({
       author: req.usuario.id,
-      name:name,
-      image:image,
-      description:description,
-      price:price,
-      countInStock:countInStock,
+      name: name,
+      image: image,
+      description: description,
+      price: price,
+      countInStock: countInStock,
     });
     const response = await model.save();
     res.status(201).send(response);
-
-
   } catch (error) {
     console.log(error);
     res.status(400).send("hubo un error en la peticion post");
   }
-
-  res.send("HOLA POST");
 };
 
-
-
-
-
 exports.PutProduct = async (req, res) => {
-  res.send("HOLA PUT");
+  try {
+    // estoy solicitando por paramentro el id ↓
+    const { idProduct } = req.params;
+    const response = await ProductModel.findByIdAndUpdate(
+      { _id: idProduct },
+      req.body,
+      { new: true }
+    );
+    res.status(201).send(response);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("hubo un error en la peticion put");
+  }
 };
 exports.DeleteProduct = async (req, res) => {
   res.send("HOLA DELETE");
 };
- 
--- src/Routes/product.js
+++ src/Routes/product.js
@@ -7,7 +7,7 @@ const ProductCollection = require("../collections/products_collections")
 
 //router + peticion ( direction(path) , + method)
 router.get('/', ProductCollection.GetProduct)
-router.get('/:idProduct', ProductCollection.GetOneProduct)
+// router.get('/:idProduct', ProductCollection.GetOneProduct)
 router.post('/',Middleware, ProductCollection.PostProduct)
 router.put('/:idProduct', ProductCollection.PutProduct)
 router.delete('/:idProduct', ProductCollection.DeleteProduct)
